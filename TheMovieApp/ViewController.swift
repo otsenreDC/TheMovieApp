a453571844ebf53714c6f57fa1bae0ec68325830
Ernesto De los Santos <ernestodlsc@gmail.com>
2018-09-24 14:04:20 -0400

    Initial Commit

---
+++ TheMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieApp
+//
+//  Created by Ernesto De los Santos Cordero on 9/24/18.
+//  Copyright © 2018 BananaLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
